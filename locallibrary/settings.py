288c5f43ee02c3b1fc1f6ec9b01142ea5e221949
laguimarfh <laguimarfh@gmail.com>
2022-11-04 22:28:12 -0400

    start django settingsboot

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -154,3 +154,6 @@ STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL= '/media/'
+MEDIA_ROOT= os.path.join(BASE_DIR, 'media')
